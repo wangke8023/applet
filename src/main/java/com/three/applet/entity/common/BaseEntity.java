5379e1a03b8bc0fcf99b58e9aee0e86c6a855f12
wangke <527767731@qq.com>
2020-05-01 16:53:30 +0800

    实体基类增加创建时间属性

--- src/main/java/com/three/applet/entity/common/BaseEntity.java
+++ src/main/java/com/three/applet/entity/common/BaseEntity.java
@@ -1,9 +1,11 @@
 package com.three.applet.entity.common;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class BaseEntity implements Serializable {
 	protected String Id;
+	protected Date createdDate;
 
 	public String getId() {
 		return Id;
@@ -11,5 +13,13 @@ public class BaseEntity implements Serializable {
 
 	public void setId(String id) {
 		Id = id;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
 	}
 }
