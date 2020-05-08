b2db10b65c906df4a3e3f77734584ecea5b7802d
wangke <527767731@qq.com>
2020-05-08 13:15:44 +0800

    用户增加手机号

--- src/main/java/com/three/applet/entity/common/user/User.java
+++ src/main/java/com/three/applet/entity/common/user/User.java
@@ -4,12 +4,20 @@ import com.three.applet.entity.common.BaseEntity;
 
 public class User extends BaseEntity {
 	private String password;
-
+	private String phoneNo;
 	public String getPassword() {
 		return password;
 	}
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
 	}
 }
