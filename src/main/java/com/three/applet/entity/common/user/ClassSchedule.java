22e635ef73db7248d4738e9e7a4636c3aaa4f9da
wangke <527767731@qq.com>
2020-05-12 12:10:30 +0800

    增加上课时间

--- src/main/java/com/three/applet/entity/common/user/ClassSchedule.java
+++ src/main/java/com/three/applet/entity/common/user/ClassSchedule.java
@@ -2,6 +2,19 @@ package com.three.applet.entity.common.user;
 
 import com.three.applet.entity.common.BaseEntity;
 
+import java.util.Date;
+
 public class ClassSchedule extends BaseEntity {
 	private static final long serialVersionUID = -4045550010561672584L;
+	private Date classTime;
+	private String teacherId;
+	private String teacherName;
+	
+	public Date getClassTime() {
+		return classTime;
+	}
+
+	public void setClassTime(Date classTime) {
+		this.classTime = classTime;
+	}
 }
