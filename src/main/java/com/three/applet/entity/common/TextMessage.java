20e70fcbd59ec93c53b0a500e49f6d689a3feceb
wangke <527767731@qq.com>
2020-05-26 13:35:26 +0800

    增加文本消息实体

---
+++ src/main/java/com/three/applet/entity/common/TextMessage.java
@@ -0,0 +1,14 @@
+package com.three.applet.entity.common;
+
+import com.three.applet.entity.common.BaseMessage;
+
+public class TextMessage extends BaseMessage {
+	private String Content;
+	public String getContent() {
+		return Content;
+	}
+	public void setContent(String content) {
+		Content = content;
+	}
+
+}
