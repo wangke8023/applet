1fdf1ad4b31f03e2006f28bf10570f1bc8c16664
wangke <527767731@qq.com>
2020-07-08 09:07:48 +0800

    结果枚举增加未登录字段

--- src/main/java/com/three/applet/commons/enums/ResultInfoEnum.java
+++ src/main/java/com/three/applet/commons/enums/ResultInfoEnum.java
@@ -6,7 +6,8 @@ public enum ResultInfoEnum {
 	//请求处理成功
 	SUCCESS("000","成功"),
 	NULL_PARAMETER("901","参数格式不正确"),
-	ILLEGAL_PARAMETER("902","必填参数为空:");
+	ILLEGAL_PARAMETER("902","必填参数为空:"),
+	NOT_LOGIN("904","未登录");
 	ResultInfoEnum(String code,String msg){
 		this.code = code;
 		this.msg =msg;
