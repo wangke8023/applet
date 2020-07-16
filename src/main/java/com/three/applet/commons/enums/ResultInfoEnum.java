1c0c981d6123190f1779b945a23bb2741a2124de
wangke <527767731@qq.com>
2020-07-16 16:08:04 +0800

    增加枚举内容

--- src/main/java/com/three/applet/commons/enums/ResultInfoEnum.java
+++ src/main/java/com/three/applet/commons/enums/ResultInfoEnum.java
@@ -7,6 +7,7 @@ public enum ResultInfoEnum {
 	SUCCESS("000","成功"),
 	NULL_PARAMETER("901","参数格式不正确"),
 	ILLEGAL_PARAMETER("902","必填参数为空:"),
+	ILLEGAL("904","非法访问"),
 	NOT_LOGIN("903","未登录");
 	ResultInfoEnum(String code,String msg){
 		this.code = code;
