fbe97edfd25d60d5f3c37f28b89dbf2f7b49ddc4
wangke <527767731@qq.com>
2021-04-19 14:04:07 +0800

    修改注释

--- sys/quartz/src/main/java/com/wwl/quartz/task/Task.java
+++ sys/quartz/src/main/java/com/wwl/quartz/task/Task.java
@@ -8,20 +8,20 @@ import com.wwl.common.utils.StringUtils;
  * 
  * @author wk
  */
-@Component("ryTask")
-public class RyTask
+@Component("task")
+public class Task
 {
-    public void ryMultipleParams(String s, Boolean b, Long l, Double d, Integer i)
+    public void multipleParams(String s, Boolean b, Long l, Double d, Integer i)
     {
         System.out.println(StringUtils.format("执行多参方法： 字符串类型{}，布尔类型{}，长整型{}，浮点型{}，整形{}", s, b, l, d, i));
     }
 
-    public void ryParams(String params)
+    public void params(String params)
     {
         System.out.println("执行有参方法：" + params);
     }
 
-    public void ryNoParams()
+    public void noParams()
     {
         System.out.println("执行无参方法");
     }
