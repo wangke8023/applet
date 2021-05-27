f8834245c9c27abde2831a98c820b663822ee3d3
wangke <527767731@qq.com>
2021-05-27 13:12:30 +0800

    修改注释内容

--- sys/common/src/main/java/com/wwl/common/utils/file/FileTypeUtils.java
+++ sys/common/src/main/java/com/wwl/common/utils/file/FileTypeUtils.java
@@ -1,7 +1,8 @@
 package com.wwl.common.utils.file;
 
-import java.io.File;
 import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
 
 /**
  * 文件类型工具类
@@ -13,7 +14,7 @@ public class FileTypeUtils
     /**
      * 获取文件类型
      * <p>
-     * 例如: ruoyi.txt, 返回: txt
+     * 例如: wwl.txt, 返回: txt
      * 
      * @param file 文件名
      * @return 后缀（不含".")
@@ -30,7 +31,7 @@ public class FileTypeUtils
     /**
      * 获取文件类型
      * <p>
-     * 例如: ruoyi.txt, 返回: txt
+     * 例如: wwl.txt, 返回: txt
      *
      * @param fileName 文件名
      * @return 后缀（不含".")
